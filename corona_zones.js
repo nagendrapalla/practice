207af3e95f66e4eadec37c3b33fbcf66fa0d091d
Nagendra Palla <nagendra.palla@outlook.com>
2020-07-11 01:11:59 +0530

    Create corona_zones.js

---
+++ corona_zones.js
@@ -0,0 +1,57 @@
+"use strict";
+
+let output = 0;
+let RG_COUNT = 0;
+const MODULO = 1000000007;
+
+function checkMaxLength(prefix, LIMITS, nop) {
+    if(nop === prefix.length) {
+        if(prefix.split("GR").length - 1 > LIMITS.AGR || prefix.split("GO").length - 1 > LIMITS.AGO) {
+            return false;
+        }
+
+        if(prefix.split("OR").length - 1 > LIMITS.AOR || prefix.split("OG").length - 1 > LIMITS.AOG) {
+            return false;
+        }
+        
+        if(prefix.split("RG").length - 1 > LIMITS.ARG || prefix.split("RO").length - 1 > LIMITS.ARO) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const nop = 1;
+const k = nop;
+const zones = ['R', 'G', 'O'];
+const zonesLength = zones.length;
+const LIMITS = {
+    ARO: 1,
+    ARG: 1,
+    AOR: 1,
+    AOG: 1,
+    AGR: 0,
+    AGO: 0
+}
+
+const start = new Date().getTime();
+let check = function(zones, prefix, n, k, limits, nop) {
+    if (k == 0) {
+        output = (output + 1) % MODULO;
+        return;
+    }
+    
+    for (let i = 0; i < n; ++i){ 
+        let newPrefix = prefix + zones[i];
+        if(newPrefix.length == 1 || (newPrefix.length > 1 && checkMaxLength(newPrefix, limits, nop))) {
+            check(zones, newPrefix, n, k - 1, limits, nop);
+        }
+    }
+};
+
+check(zones, "", zonesLength, k, LIMITS, nop);
+const end = new Date().getTime();
+const time = (end - start) / 1000;
+console.log(time)
+
+console.log(output);
